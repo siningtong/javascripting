e4a8a262881565761b8954ad01d03cf0dcf35572
siningtong <siningtong@gmail.com>
2019-08-21 04:35:26 +0000

    compare

---
+++ w1/lotide/eqArrays.js
@@ -0,0 +1,18 @@
+const assertEqual=function(actual,expected){
+  if (actual===expected){
+      console.log(`😀Assertion Passed: ${actual} === ${expected}`)
+  }
+  else{
+      console.log(`😂Assertion Failed: ${actual} !== ${expected}`)
+  }
+  };
+
+
+  const eqArrays=function(array1, array2){
+    for(let i = 0; i < array1.length; i++){
+      if(array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
