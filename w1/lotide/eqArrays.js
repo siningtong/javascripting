33decf95f5dd7bc7bb872a185500f93b1455e797
siningtong <siningtong@gmail.com>
2019-08-22 00:21:31 +0000

    updated

--- w1/lotide/eqArrays.js
+++ w1/lotide/eqArrays.js
@@ -6,9 +6,13 @@ const assertEqual=function(actual,expected){
       console.log(`😂Assertion Failed: ${actual} !== ${expected}`)
   }
   };
-
-
   const eqArrays=function(array1, array2){
+    if (!array1||!array2){
+      return false
+    }
+    if(array1.length!==array2.length){
+      return false
+    }
     for(let i = 0; i < array1.length; i++){
       if(array1[i] !== array2[i]) {
         return false;
@@ -16,3 +20,11 @@ const assertEqual=function(actual,expected){
     }
     return true;
   }
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]))
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+  
+assertEqual(eqArrays(['a','aswe'],[1,'rsa','ase']),false)
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+  
+  eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+  eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
