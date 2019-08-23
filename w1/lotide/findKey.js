db9a81d28e4807a35e55f5f7779238a3a3dc9354
siningtong <siningtong@gmail.com>
2019-08-23 04:17:21 +0000

    findKey

---
+++ w1/lotide/findKey.js
@@ -0,0 +1,30 @@
+const assertEqual=function(actual,expected){
+  if (actual===expected){
+      console.log(`😀Assertion Passed: ${actual} === ${expected}`)
+  }
+  else{
+      console.log(`😂Assertion Failed: ${actual} !== ${expected}`)
+  }
+  }
+  
+
+
+
+
+  const findKey = function(object,callback){
+for(let keys in object){
+  if(callback(object[keys])){
+    return keys;
+  }
+}
+  }
+  console.log(findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2))
+  
+  
